70957c547b3cdb24bacff19f0fb27d32ed5e92e1
kabegamikamio <kabegamikamio@gmail.com>
2024-11-19 14:36:56 +0900

    added IndexController

--- src/main/java/urlshortener/main/urlshortener/IndexController.java
+++ src/main/java/urlshortener/main/urlshortener/IndexController.java
@@ -1,4 +1,19 @@
 package urlshortener.main.urlshortener;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String accessUrl(@PathVariable() String id) {
+        System.out.println(id);
+        return "index";
+    }
 }
